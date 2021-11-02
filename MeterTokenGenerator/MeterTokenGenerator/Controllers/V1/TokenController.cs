2b222a5d23a7b585cdecb0b0e3dd1d51accf87ef
HamidovOrkhn <orxan.hamidov.orxan.hamidov@mail.ru>
2021-11-02 09:22:38 +0400

    update api

--- MeterTokenGenerator/MeterTokenGenerator/Controllers/V1/TokenController.cs
+++ MeterTokenGenerator/MeterTokenGenerator/Controllers/V1/TokenController.cs
@@ -29,8 +29,49 @@ namespace MeterTokenGenerator.V1.Controllers
             catch (Exception)
             {
                 return StatusCode(500, "Data Not Decrypted");
+
             }
       
+        }
+        [HttpGet("[action]/{amount}")]
+        public IActionResult DebtCredit(int amount)
+        {
+            try
+            {
+                return Ok(_generator.BulkToToken(new BulkRequest { Borc_Kredi = amount.ToString() }));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Data Not Decrypted");
+
+            }
+
+        }
+        [HttpGet("[action]/{amount}")]
+        public IActionResult MaxPowerLimit(int amount)
+        {
+            try
+            {
+                return Ok(_generator.BulkToToken(new BulkRequest { Max_Power_Limit = amount.ToString() }));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Data Not Decrypted");
+            }
+
+        }
+        [HttpPost("[action]")]
+        public IActionResult TariffChange([FromBody]Tariff tariff)
+        {
+            try
+            {
+                return Ok(_generator.BulkToToken(new BulkRequest { Tariff = tariff }));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Data Not Decrypted");
+            }
+
         }
     }
 }
