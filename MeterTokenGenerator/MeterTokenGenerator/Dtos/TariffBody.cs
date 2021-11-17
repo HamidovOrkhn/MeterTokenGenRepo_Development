df7e95b19f35a1203c7a1bfde89b841065ccde79
HamidovOrkhn <orxan.hamidov.orxan.hamidov@mail.ru>
2021-11-17 10:40:17 +0400

    update payment service

---
+++ MeterTokenGenerator/MeterTokenGenerator/Dtos/TariffBody.cs
@@ -0,0 +1,13 @@
+﻿using StsTokenGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeterTokenGenerator.Dtos
+{
+    public class TariffBody
+    {
+        public Tariff Tariff { get; set; }
+    }
+}
